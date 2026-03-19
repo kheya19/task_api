fc3553fc63111713d6a757caa526bedb6a48de64
Kheya Das <kheyadas1919@gmail.com>
2026-03-19 19:45:54 +0530

    added get task by id file

--- handlers/get_task_by_id.go
+++ handlers/get_task_by_id.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/kheya19/task_api/database"
+	"github.com/kheya19/task_api/model"
+)
+
+func GetTaskByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var task model.Task
+	result := database.DB.Where("id = ?", id).First(&task)
+	if result.Error != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "task not found",
+		})
+	}
+	return c.JSON(task)
+}
